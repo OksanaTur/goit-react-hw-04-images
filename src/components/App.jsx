bb3c04168c73611ea1f6528a68ca9b2091d75c4a
OksanaTur <ksyturkevych@gmail.com>
2023-03-10 15:16:14 +0400

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Component } from "react";
+import React, { useEffect, useState } from 'react';
 import { Searchbar } from "./Searchbar/Searchbar";
 import { fetchImages } from "./Api/fetchImages";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
@@ -9,101 +8,89 @@ import { Modal } from "./Modal/Modal";
 
 
 
-export class App extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    currentSearch: '',
-    pageNr: 1,
-    modalOpen: false,
-    modalImg: '',
-    modalAlt: '',
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentSearch, setCurrentSearch] = useState('');
+  const [pageNr, setPageNr] = useState(1);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalImg, setModalImg] = useState('');
+  const [modalAlt, setModalAlt] = useState('');
  
    
-    handleSubmit = async e => {
-      e.preventDefault();
-      this.setState({ isLoading: true });
-      const inputForSearch = e.target.elements.inputForSearch;
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setIsLoading(true);
+    const inputForSearch = e.target.elements.inputForSearch;
    
-      const response = await fetchImages(inputForSearch.value, 1);
-      this.setState({
-        images: response,
-        isLoading: false,
-        currentSearch: inputForSearch.value,
-        pageNr: 1,
-      });
-    };
+    const response = await fetchImages(inputForSearch.value, 1);
+    setImages(response);
+    setIsLoading(false);
+    setCurrentSearch(inputForSearch.value);
+    setPageNr(1);
+  };
+  
 
-    handleClickMore = async () => {
-      const response = await fetchImages(
-        this.state.currentSearch,
-        this.state.pageNr + 1
-      );
-      this.setState({
-        images: [...this.state.images, ...response],
-        pageNr: this.state.pageNr + 1,
-      });
-    };
+   const handleClickMore = async () => {
+    setIsLoading({ isLoading: true });
+    const response = await fetchImages(currentSearch, pageNr);
+    setImages([...images, ...response]);
+    setIsLoading(false);
+    setPageNr(pageNr + 1);
+  };
 
-    handleImageClick = e => {
-      this.setState({
-        modalOpen: true,
-        modalAlt: e.target.alt,
-        modalImg: e.target.name,
-      });
-    };
+  const handleImageClick = e => {
+    setModalOpen(true);
+    setModalAlt(e.target.alt);
+    setModalImg(e.target.name);
+  };
 
-    handleModalClose = () => {
-      this.setState({
-        modalOpen: false,
-        modalImg: '',
-        modalAlt: '',
-      });
-    };
-
-    handleKeyDown = event => {
+  const handleModalClose = () => {
+    setModalOpen(false);
+    setModalImg('');
+    setModalAlt('');
+  };
+ 
+  useEffect(() => {
+    const handleKeyDown = event => {
       if (event.code === 'Escape') {
-        this.handleModalClose();
+        handleModalClose();
       }
     };
-
-  async componentDidMount() {
-      window.addEventListener('keydown', this.handleKeyDown);
-    }
-
-    render() {
-      return (
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: '1fr',
-            gridGap: '16px',
-            paddingBottom: '24px',
-          }}
-        >
-          {this.state.isLoading ? (
-            <Loader />
-          ) : (
-            <React.Fragment>
-              <Searchbar onSubmit={this.handleSubmit} />
-              <ImageGallery
-                onImageClick={this.handleImageClick}
-                images={this.state.images}
-              />
-              {this.state.images.length > 0 ? (
-                <Button onClick={this.handleClickMore} />
-              ) : null}
-            </React.Fragment>
-          )}
-          {this.state.modalOpen ? (
-            <Modal
-              src={this.state.modalImg}
-              alt={this.state.modalAlt}
-              handleClose={this.handleModalClose}
-            />
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+    
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridGap: '16px',
+        paddingBottom: '24px',
+      }}
+    >
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <React.Fragment>
+          <Searchbar onSubmit={handleSubmit} />
+          <ImageGallery
+            onImageClick={handleImageClick}
+            images={images}
+          />
+          {images.length > 0 ? (
+            <Button onClick={handleClickMore} />
           ) : null}
-        </div>
-      )
-    }
-  }
+        </React.Fragment>
+      )}
+      {modalOpen ? (
+        <Modal
+          src={modalImg}
+          alt={modalAlt}
+          handleClose={handleModalClose}
+        />
+      ) : null}
+    </div>
+  )
+};
+  
